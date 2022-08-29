962e652c5f679bbbef3362c17e94d77d11450bd7
ackerman <dharmiegra4@gmail.com>
2022-08-29 14:59:18 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Profile from './components/Profile';
 
-function App() {
+
+
+
+const App =()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='Heading'>Random User Profile</h1>
+      <Profile></Profile>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Profile.js
@@ -0,0 +1,68 @@
+
+
+import React, { useEffect, useState } from 'react'
+import { faker } from '@faker-js/faker';
+
+
+
+const Profile = ()=> {
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [birthday, setBirthday] = useState("")
+    const [image,setImage] = useState("")
+    const[number, setNumber]=useState("")
+
+    useEffect ( () => {
+      getData()
+    } , [])
+    var randomProfile = require('random-profile-generator');
+    const getData=()=>{
+   let userProfile=  randomProfile.profile()
+  
+        let img = faker.internet.avatar();
+        let fName = userProfile.firstName
+        let lName = userProfile.lastName
+        let mail =userProfile.email
+        let birthday = userProfile.birthday
+        let number= userProfile.phone
+  
+  
+        setFirstName(fName)
+        setLastName(lName)
+        setImage(img)
+        setBirthday(birthday)
+        setEmail(mail)
+        setNumber(number)
+        console.log(userProfile);
+    }
+
+
+  const handle = ({isActive,clicked}) => {
+    getData()
+  }
+
+
+  return (
+    <div className="App">
+
+      <div className="card-box">
+      <div className='img'><img src={image} alt="ProfileImage"/></div>
+      <div className='details'>
+      <h2> First name :  {firstName}</h2>
+      <h2> Last Name: {lastName}</h2>
+      <h2 className='email'> Email: {email}</h2>
+      <h2 className='country'> Date of Birth: {birthday}</h2>
+      <h2 className='number'> Phone Number: {number}</h2>
+      <button className='btn btn-outline-primary createUser' onClick={handle}>Get another User</button>
+      </div>
+      </div>
+     
+     
+     
+    </div>
+
+  )
+}
+
+export default Profile
